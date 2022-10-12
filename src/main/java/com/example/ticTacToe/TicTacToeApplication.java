f86f4385fad4368edc78dba868a8fe10edcfcfca
Alexander Shpin <@lexhpin@gmail.com>
2022-10-12 14:49:54 -0500

    Initial setup

---
+++ src/main/java/com/example/ticTacToe/TicTacToeApplication.java
@@ -0,0 +1,21 @@
+package com.example.ticTacToe;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToeApplication extends Application {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(TicTacToeApplication.class);
+    }
+}
